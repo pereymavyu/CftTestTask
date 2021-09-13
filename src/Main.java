b96451ddffb292956ec4f34ef3e63aef1697abb4
pereymavyu <pereyma.ic@gmail.com>
2021-09-13 08:50:21 +0600

    generify sort method

--- src/Main.java
+++ src/Main.java
@@ -95,8 +95,81 @@ public class Main {
         }
     }
 
+    public static <T> void sort(String[] inputFileNames, String outputFileName, DataType dataType, SortingOrder sortingOrder) throws IOException {
+        int inputFilesAmount = inputFileNames.length;
 
-    public static void sort(String[] inputFileNames, String outputFileName, DataType dataType, SortingOrder sortingOrder) throws IOException {
+        ArrayList<BufferedReader> input = new ArrayList<>();
+
+        for (String inputFileName : inputFileNames) {
+            input.add(new BufferedReader(new FileReader(inputFileName)));
+        }
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(outputFileName));
+
+        ArrayList<Comparable> valuesToCompare = new ArrayList<>();
+
+        String currentLine;
+
+        if (dataType == DataType.STRING) {
+            for (int i = 0; i < input.size(); ++i) {
+                if ((currentLine = input.get(i).readLine()) != null) {
+                    valuesToCompare.add(currentLine);
+                } else {
+                    input.get(i).close();
+                    input.remove(i);
+                }
+            }
+        }
+
+        if (dataType == DataType.INTEGER) {
+            for (int i = 0; i < input.size(); ++i) {
+                if ((currentLine = input.get(i).readLine()) != null) {
+                    valuesToCompare.add(Integer.parseInt(currentLine));
+                } else {
+                    input.get(i).close();
+                    input.remove(i);
+                }
+            }
+        }
+
+        Comparable min = valuesToCompare.get(0);
+        int minIndex = 0;
+
+        while (input.size() > 0) {
+            for (int i = 0; i < input.size(); ++i) {
+
+                if (valuesToCompare.get(i).compareTo(min) > 0) {
+                    minIndex = i;
+                    min = valuesToCompare.get(i);
+                }
+            }
+
+            bw.write(String.valueOf(min));
+            bw.newLine();
+
+            String temp;
+
+            if ((temp = input.get(minIndex).readLine()) != null) {
+                if (dataType == DataType.STRING) {
+                    valuesToCompare.add(temp);
+                } else if (dataType == DataType.INTEGER) {
+                    valuesToCompare.add(Integer.parseInt(temp));
+                }
+            } else {
+                input.get(minIndex).close();
+                input.remove(minIndex);
+
+
+            }
+        }
+    }
+}
+
+
+    /*
+
+
+    public static <T> void sort(String[] inputFileNames, String outputFileName, DataType dataType, SortingOrder sortingOrder) throws IOException {
         int inputFilesAmount = inputFileNames.length;
 
         BufferedReader[] input = new BufferedReader[inputFilesAmount];
@@ -162,5 +235,9 @@ public class Main {
 
 
         }
+
+
     }
 }
+
+     */
